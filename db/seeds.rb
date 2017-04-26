22d12aeaecd0b9855eeecc9d6027cd6f924c50cd
Tyrant Davis <davistyrant@gmail.com>
2017-04-26 14:02:34 -0700

    Create seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,45 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+# Create Member
+member = User.create(
+  name: 'Member',
+  email: "member@example.com",
+  password: 'password'
+)
+member.skip_confirmation!
+member.save!
+
+#Create users
+5.times do
+  user = User.create(
+    name: Faker::Name.name,
+    email:  Faker::Internet.email
+    password: "password"
+    )
+    user.skip_confirmation!
+    user.save!
+end
+users = User.all
+
+# Create registered Applications
+5.times do
+  RegisteredApplication.create!(
+    user: users.sample,
+    name: Faker::App.name,
+    url: Faker::Internet.url
+  )
+end
+applcations = RegisteredApplication.all
+
+# Create Events
+events = ['scroll', 'click', 'register', 'download', 'vist']
+25.times do
+  Event.create!(
+    registered_application: applcations.sample,
+    name: events.sample
+  )
+end
+
+puts "Seed finished"
+puts "#{RegisteredApplication.count} applcations created"
+puts "#{Event.count} events created"
