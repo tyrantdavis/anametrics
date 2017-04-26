5be12302bf1f7a300e90879e5b58c4d40acdac83
Tyrant Davis <davistyrant@gmail.com>
2017-04-26 11:16:03 -0700

    Set model up to add role to users. Add validations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,21 @@
 class User < ApplicationRecord
-  has_many :registeredapplications, dependent: :destroy
+  has_many :registered_applications, dependent: :destroy
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable, :lockable
 
 before_save {self.email.downcase if email.present?}
 # before_save {self.name.capitalize if name.present?}
-before_save {self.role ||= :user}
+# before_save {self.role ||= :user}
 
-enum role: [:user, :admin]
+# enum role: [:user, :admin]
 
 # validates :name, length: {minimum: 1, maximum: 100}, presence: true
 validates :email, presence: true, uniqueness: {case_sensitive: false},
 length: {minimum: 3, maximum: 254}
 validates :password, presence: true, length: {minimum: 6}
-
+# validates :password_confirmation, presence: true
+# validates_associated :registered_applications
 protected
   def confirmation_required?
     false
