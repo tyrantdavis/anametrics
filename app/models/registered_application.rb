3d00b4a28dff8a73e0f53fd023722141bd6eebf6
Tyrant Davis <davistyrant@gmail.com>
2017-04-26 11:09:11 -0700

    Add validations. Set descending order.

--- app/models/registered_application.rb
+++ app/models/registered_application.rb
@@ -2,5 +2,7 @@ class RegisteredApplication < ApplicationRecord
   belongs_to :user
 
   validates :user_id, presence: true
-
+  validates :name, presence: true, length: {minimum: 1, maximum: 255}
+  validates :url, presence: true
+  default_scope { order('created_at DESC') }
 end
