663ad6d403ecc7de87396e387de6f2d2a9e8d9db
Tyrant Davis <davistyrant@gmail.com>
2017-04-26 11:03:33 -0700

    Config and sanitize permitted params. Add login helper and method.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,24 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user
-
+  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
 
 
 protected
 
-def configure_permitted_parameters
-    added_attrs = [:email, :password, :password_confirmation, :remember_me]
-    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
-    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
-end
+  def configure_permitted_parameters
+      added_attrs = [:email, :password, :password_confirmation, :remember_me]
+      devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+      devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+    end
+
+private
+
+  def require_sign_in
+    unless current_user
+      flash[:alert] = "You must be signed in to do that."
+      redirect_to new_user_session_path
+    end
+  end
 end
