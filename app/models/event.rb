0dc056c76c35b41dc67b26535a4494eed9a326d5
Tyrant Davis <davistyrant@gmail.com>
2017-04-26 12:28:19 -0700

    Generate event model.

---
+++ app/models/event.rb
@@ -0,0 +1,6 @@
+class Event < ApplicationRecord
+  belongs_to :registered_application
+
+  validates :registered_application_id, presence: true
+  validates :name, presence: true, length: {minimum: 1, maximum: 255}
+end
