343ce23bff56001b5d1615a7e3ec99602872a905
Tyrant Davis <davistyrant@gmail.com>
2017-04-26 10:52:13 -0700

    Update routes. Remove unnecessary routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,34 +1,13 @@
 Rails.application.routes.draw do
-  get 'users/index'
 
-  get 'users/new'
+devise_for :users
+resources :registered_applications
+resources :user, only: [:show]
 
-  get 'users/create'
+authenticated :user do
+      root 'users#show', as: :authenticated_root
+end
 
-  get 'users/show'
-
-  get 'users/edit'
-
-  get 'users/update'
-
-  get 'users/destroy'
-
-  get 'registered_applications/index'
-
-  get 'registered_applications/new'
-
-  get 'registered_applications/create'
-
-  get 'registered_applications/show'
-
-  get 'registered_applications/edit'
-
-  get 'registered_applications/update'
-
-  get 'registered_applications/destroy'
-
-  devise_for :users
   get 'welcome/index'
   root to: 'welcome#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
